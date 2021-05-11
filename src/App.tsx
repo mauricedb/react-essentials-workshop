357973023232390820be1d89f9b9ece4a02f490f
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-05-11 15:40:04 +0200

    Styling with Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Greeter } from "./components/Greeting";
 
 function App() {
   return (
-    <div className="App">
+    <div
+      className="container"
+      style={{ backgroundColor: "rgba(0, 0, 255, 0.05)" }}
+    >
       <Greeter firstName="Maurice" today={new Date()} />
     </div>
   );
